8a7a319c8409dbb502cdef797689256af1b193a5
Sayandeep <sdroy001@gmail.com>
2025-09-13 00:14:17 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Surprise from "./pages/Surprise";
 import NotFound from "./pages/NotFound";
@@ -14,14 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router> {/* Changed from BrowserRouter to Router */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/surprise" element={<Surprise />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router> {/* Changed from BrowserRouter to Router */}
     </TooltipProvider>
   </QueryClientProvider>
 );
